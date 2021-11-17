06513a702a71ac64e7b2161b2bdb7566ace69cae
上野git config --global user.name 上野太慎 <taishin.jr@gmail.com>
2021-11-17 08:52:46 +0000

    rails5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users
 end
